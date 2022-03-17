b81642ac83b9d31a77d64d965ac4faca4bb4e766
fred <310bel@gmail.com>
2022-03-18 00:32:16 +0300

    дома

--- resources/views/schedule/schedule.blade.php
+++ resources/views/schedule/schedule.blade.php
@@ -6,22 +6,36 @@
         use App\Components\ImportDataClient;
         $import = new ImportDataClient();
         $response = $import->client->request('GET', '');
-        $data = json_decode($response->getBody()->getContents(),TRUE);
+        $data = json_decode($response->getBody());
 
-        $kitchen = array("Spoons"=>"Spoons", "Knifes"=>"Knifes", "Plates"=>"Plates", "Plates2"=>"Spoons");
+    $depart = array
+                (
+                    "1101" => "Юридический институт",
+                    "110201" => "Факультет физической культуры педагогического института",
+                    "110202" => "Факультет дошкольного, начального и специального образования педагогического института",
+                    "110203" => "Историко-филологический факультет педагогического института",
+                    "110204" => "Факультет математики и естественнонаучного образования педагогического института",
+                    "110205" => "Факультет иностранных языков педагогического института",
+                    "110206" => "Факультет психологии педагогического института",
+                    "1103" => "Медицинский институт",
+                    "110305" => "Медицинский колледж Медицинского института",
+                    "110307" => "Факультет медико-профилактического дела медицинского института",
+                    "1104" => "Институт межкультурной коммуникации и международных отношений",
+                    "1108" => "Институт наук о Земле",
+                    "1109" => "1109",
+                );
 
-        foreach ($data as $item) {
+foreach ($data as $key=> $item){
+
 echo '<pre>';
-print_r($item);
-echo '</pre>';}
+print_r($item[0]->subdepteacher); // вывод свойства у обьекта $item[0] (параметр true у json_decode нету)
+//print_r($item); // вывод
+//print_r($item2['groups']); // вывод элемента из ассоциативного массива вложенного в другой(нужно будет добавить вложеный цикл         foreach ($item as $item2) {}
+echo '</pre>';
+print_r($item[0]->teacher); // вывод свойства у обьекта $item[0] (параметр true у json_decode нету)
 
-/*foreach($data as $brand => $item)
-{
-foreach($item  as  $inner_key => $value)
-{
-echo "[$brand][$inner_key] = $value";
 }
-}*/
+
 // dd($item);
 
     @endphp
@@ -60,159 +74,159 @@ echo "[$brand][$inner_key] = $value";
     </div>
 
     <div id='content' class="tab-content">
-    <!----------------------------------------------------------------------------------------------------------------->
-    <!-- РАСПИСАНИЕ СТУДЕНТОВ   --------------------------------------------------------------------------------------->
-    <!----------------------------------------------------------------------------------------------------------------->
+        <!----------------------------------------------------------------------------------------------------------------->
+        <!-- РАСПИСАНИЕ СТУДЕНТОВ   --------------------------------------------------------------------------------------->
+        <!----------------------------------------------------------------------------------------------------------------->
 
 
-    <div class="tab-pane fade show in active" id="home">
-        <!--        <center><font size="5" color="green"> Уважаемые студенты! Расписание находится в стадии формирования.</font></center><br>
-        -->        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <select class="form-control" name="facultys" onchange="loadGroups(this)" style="max-width: 434px;">
-                        <option>Выберите факультет</option>
+        <div class="tab-pane fade show in active" id="home">
+            <!--        <center><font size="5" color="green"> Уважаемые студенты! Расписание находится в стадии формирования.</font></center><br>
+            -->        <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <select class="form-control" name="facultys" onchange="loadGroups(this)" style="max-width: 434px;">
+                            <option>Выберите факультет</option>
 
-                                                <? foreach ($kitchen as $item) {
-                                                    echo '<option >'.$item.'</option>';
-                                                } ?>
+                            <? foreach ($depart as $key=> $item) {
+                                echo '<option >'. $key .". ". $item.'</option>';
+                            } ?>
 
-                    </select>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="container" >
-            <div class="row justify-content-center">
-                <div class="col-md-5" style="margin-top: 25px;">
-                    <select class="form-control"  id="groupList" name="groups[]" onchange="loadSchedule(this)" style="max-width: 434px;">
-                        <option value="0">Для отображения списка групп выберите факультет</option>
-                    </select>
-                </div>
-
-                <div class="offset-md-2" style="margin-top: 25px;"></div>
-
-                <div class="col-md-2" style="margin-top: 25px;">
-                    <input type="tel" autocorrect="off" pattern="\d [0-9]" class="form-control grpsrch" id="groupsearch" type="text" placeholder="Введите группу" style="height: 30px;">
-                </div>
-
-                <div class="col-md-3" style="margin-top: 25px;">
-                    <button class="btn btn-outline-info" id="btnsearch" type="button">Поиск</button>
-                    <!--                            01001803-->
-                    {{--
-                                        <script>
-                                            function someFunc2() {
-                                                var newtime = <?echo $newTime?>;
-                                                if (document.getElementById("groupsearch").value == '')
-                                                    return false;
-                                                else loadSchedule2(document.getElementById("groupsearch").value, newtime);
-                                            }
-                                            document.getElementById("btnsearch").onclick = someFunc2;
-                                        </script>
-                    --}}
-                </div>
-            </div>
-        </div>
-
-        <div class="container" style="margin-top: 25px;">
-            <div class="row ">
-                <div class="col-md-12">
-                    <div class="d-sm-none"> <!-- Для мобильных-->
-                        <nav aria-label="Page navigation">
-                            <ul class="pagination justify-content-center">
-                                <li id="week" data-val="prevWeek" class="page-item">
-                                    <a class="page-link" href="#">
-                                        <i class="fas fa-arrow-circle-left"></i>
-                                    </a>
-                                </li>
-                                <li id="week" data-val="thisWeek" class="page-item">
-                                    <a class="page-link" href="#">Текущая неделя</a>
-                                </li>
-                                <li id="week" data-val="nextWeek" class="page-item">
-                                    <a class="page-link" href="#">
-                                        <i class="fas fa-arrow-circle-right"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
+                        </select>
                     </div>
-                    <div class="d-none d-sm-inline-block d-md-none">
-                        <nav aria-label="Page navigation">
-                            <ul class="pagination justify-content-center">
-                                <li id="week" data-val="prevWeek" class="page-item">
-                                    <a class="page-link" href="#">
-                                        <i class="fas fa-arrow-circle-left"></i>Пред. неделя
-                                    </a>
-                                </li>
-                                <li id="week" data-val="thisWeek" class="page-item">
-                                    <a class="page-link" href="#">Текущая неделя</a>
-                                </li>
-                                <li id="week" data-val="nextWeek" class="page-item">
-                                    <a class="page-link" href="#">
-                                        <i class="fas fa-arrow-circle-right"></i>След. неделя
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <!--
-                    <div class="d-none d-md-block">
-                        <nav aria-label="Page navigation">
-                            <ul class="pagination justify-content-center">
-                                <li id="week" data-val="prevWeek" class="page-item"><a class="page-link" href="#"><i
-                                                class="fas fa-arrow-circle-left"></i> Предыдущая неделя</a></li>
-                                <li id="week" data-val="thisWeek" class="page-item"><a class="page-link" href="#">Текущая
-                                        неделя</a></li>
-                                <li id="week" data-val="nextWeek" class="page-item"><a class="page-link" href="#">Следующая
-                                        неделя <i class="fas fa-arrow-circle-right"></i></a></li>
-                            </ul>
-                        </nav>
-                    </div>-->
-
                 </div>
             </div>
-        </div>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div id="stbl">
+
+            <div class="container" >
+                <div class="row justify-content-center">
+                    <div class="col-md-5" style="margin-top: 25px;">
+                        <select class="form-control"  id="groupList" name="groups[]" onchange="loadSchedule(this)" style="max-width: 434px;">
+                            <option value="0">Для отображения списка групп выберите факультет</option>
+                        </select>
+                    </div>
+
+                    <div class="offset-md-2" style="margin-top: 25px;"></div>
+
+                    <div class="col-md-2" style="margin-top: 25px;">
+                        <input type="tel" autocorrect="off" pattern="\d [0-9]" class="form-control grpsrch" id="groupsearch" type="text" placeholder="Введите группу" style="height: 30px;">
+                    </div>
+
+                    <div class="col-md-3" style="margin-top: 25px;">
+                        <button class="btn btn-outline-info" id="btnsearch" type="button">Поиск</button>
+                        <!--                            01001803-->
+                        {{--
+                                            <script>
+                                                function someFunc2() {
+                                                    var newtime = <?echo $newTime?>;
+                                                    if (document.getElementById("groupsearch").value == '')
+                                                        return false;
+                                                    else loadSchedule2(document.getElementById("groupsearch").value, newtime);
+                                                }
+                                                document.getElementById("btnsearch").onclick = someFunc2;
+                                            </script>
+                        --}}
+                    </div>
+                </div>
+            </div>
+
+            <div class="container" style="margin-top: 25px;">
+                <div class="row ">
+                    <div class="col-md-12">
+                        <div class="d-sm-none"> <!-- Для мобильных-->
+                            <nav aria-label="Page navigation">
+                                <ul class="pagination justify-content-center">
+                                    <li id="week" data-val="prevWeek" class="page-item">
+                                        <a class="page-link" href="#">
+                                            <i class="fas fa-arrow-circle-left"></i>
+                                        </a>
+                                    </li>
+                                    <li id="week" data-val="thisWeek" class="page-item">
+                                        <a class="page-link" href="#">Текущая неделя</a>
+                                    </li>
+                                    <li id="week" data-val="nextWeek" class="page-item">
+                                        <a class="page-link" href="#">
+                                            <i class="fas fa-arrow-circle-right"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="d-none d-sm-inline-block d-md-none">
+                            <nav aria-label="Page navigation">
+                                <ul class="pagination justify-content-center">
+                                    <li id="week" data-val="prevWeek" class="page-item">
+                                        <a class="page-link" href="#">
+                                            <i class="fas fa-arrow-circle-left"></i>Пред. неделя
+                                        </a>
+                                    </li>
+                                    <li id="week" data-val="thisWeek" class="page-item">
+                                        <a class="page-link" href="#">Текущая неделя</a>
+                                    </li>
+                                    <li id="week" data-val="nextWeek" class="page-item">
+                                        <a class="page-link" href="#">
+                                            <i class="fas fa-arrow-circle-right"></i>След. неделя
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <!--
+                        <div class="d-none d-md-block">
+                            <nav aria-label="Page navigation">
+                                <ul class="pagination justify-content-center">
+                                    <li id="week" data-val="prevWeek" class="page-item"><a class="page-link" href="#"><i
+                                                    class="fas fa-arrow-circle-left"></i> Предыдущая неделя</a></li>
+                                    <li id="week" data-val="thisWeek" class="page-item"><a class="page-link" href="#">Текущая
+                                            неделя</a></li>
+                                    <li id="week" data-val="nextWeek" class="page-item"><a class="page-link" href="#">Следующая
+                                            неделя <i class="fas fa-arrow-circle-right"></i></a></li>
+                                </ul>
+                            </nav>
+                        </div>-->
 
                     </div>
                 </div>
             </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div id="stbl">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
 
-    </body>
+        </body>
 
-    <footer class="row">
-        <div class="container" style="margin-top: 20px;">
+        <footer class="row">
+            <div class="container" style="margin-top: 20px;">
             <span class="transformer-tabs">
                 <a href="http://www.bsu.edu.ru/bsu/structure/detail.php?ID=2263">Ресурс департамента образовательной политики</a>
                 <br>Разработка и техническая поддержка: E-mail:
                 <a href="mailto:DekanatAdm@bsu.edu.ru?subject=ИнфоБелГУ 2013">DekanatAdm@bsu.edu.ru</a>
             </span>
-        </div>
-    </footer>
+            </div>
+        </footer>
 
 
-    </html>
+        </html>
 
 
 
 
 
-    <p>
-        Выберете группу
-        <select name="formGender">
-            <option value="">Select...</option>
-            <option value="M">Male</option>
+        <p>
+            Выберете группу
+            <select name="formGender">
+                <option value="">Select...</option>
+                <option value="M">Male</option>
 
-            <option value="F">Female</option>
-        </select>
-    </p>
+                <option value="F">Female</option>
+            </select>
+        </p>
 
 
 @endsection
