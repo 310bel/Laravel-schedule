165472806807b4fedbd232c094ee0cb9ea1add3e
Ilchenko E <ilchenko_e@bsu.edu.ru>
2022-03-21 16:58:03 +0300

    подставил факультеты из API в селектор

--- app/Components/ImportDataClient.php
+++ app/Components/ImportDataClient.php
@@ -15,7 +15,8 @@ public $client;
     {
         $this->client = new Client([
             // Base URI is used with relative requests https://reqres.in/api/
-            'base_uri' => 'https://dekanat.bsu.edu.ru/blocks/bsu_api/bsu_schedule/readStudent.php?os=android&form=0&group=12002101&date=17.03.2022',
+            'base_uri' => 'https://dekanat.bsu.edu.ru/blocks/bsu_api/bsu_schedule/readStudent.php?os=android',
+            //https://dekanat.bsu.edu.ru/blocks/bsu_api/bsu_schedule/readStudent.php?os=android&form=0&group=12002101&date=17.03.2022
             // You can set any number of default request options.
             'timeout'  => 2.0,
            // 'verify' => false,
